8a1d1b0aa5ce2924c786f35475833b42f88497db
Sergey <sergey.u.severin@gmail.com>
2019-08-26 12:16:54 +0300

    work on admin page

--- config/constants.php
+++ config/constants.php
@@ -2,7 +2,7 @@
 return [
     'db' => [
         'roles' => [
-            'admin' => 'Admin',
+            'admin' => 'admin',
             'customer' => 'Customer'
         ],
         'order_statuses' => [
